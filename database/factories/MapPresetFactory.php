8b499075c5fa3c5ff329f86a727e325ba313a802
Alex Goodwin <alex.goodwin@multitel.com.au>
2019-01-20 12:52:59 +1000

    Supply isOpened field in MapPresetFactory

--- database/factories/MapPresetFactory.php
+++ database/factories/MapPresetFactory.php
@@ -6,5 +6,6 @@ $factory->define(App\Models\MapPreset::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'uuid' => $faker->uuid,
+        'isOpened' => rand(0, 1),
     ];
 });
