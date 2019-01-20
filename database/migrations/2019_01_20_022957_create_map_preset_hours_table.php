f7d8c158ec2fab8e80e4951d8f86670c1ce91348
Alex Goodwin <alex.goodwin@multitel.com.au>
2019-01-20 16:14:46 +1000

    Add existence guards to regenerated migrations

--- database/migrations/2019_01_20_022957_create_map_preset_hours_table.php
+++ database/migrations/2019_01_20_022957_create_map_preset_hours_table.php
@@ -13,26 +13,28 @@ class CreateMapPresetHoursTable extends Migration
      */
     public function up()
     {
-        Schema::create('map_preset_hours', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('map_preset_id')->index();
-            $table->integer('open_period_mins');
-            $table->integer('close_period_mins');
-            $table->string('repeat');
-            $table->boolean('wd_0')->default(false);
-            $table->boolean('wd_1')->default(false);
-            $table->boolean('wd_2')->default(false);
-            $table->boolean('wd_3')->default(false);
-            $table->boolean('wd_4')->default(false);
-            $table->boolean('wd_5')->default(false);
-            $table->boolean('wd_6')->default(false);
-            $table->string('dates')->nullable();
-            $table->json('days')->nullable();
+        if(!Schema::hasTable('map_preset_hours')) {
+            Schema::create('map_preset_hours', function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('map_preset_id')->index();
+                $table->integer('open_period_mins');
+                $table->integer('close_period_mins');
+                $table->string('repeat');
+                $table->boolean('wd_0')->default(false);
+                $table->boolean('wd_1')->default(false);
+                $table->boolean('wd_2')->default(false);
+                $table->boolean('wd_3')->default(false);
+                $table->boolean('wd_4')->default(false);
+                $table->boolean('wd_5')->default(false);
+                $table->boolean('wd_6')->default(false);
+                $table->string('dates')->nullable();
+                $table->json('days')->nullable();
 
-            $table->timestampsTz();
-            $table->softDeletesTz();
-            $table->foreign('map_preset_id')->references('id')->on('map_presets');
-        });
+                $table->timestampsTz();
+                $table->softDeletesTz();
+                $table->foreign('map_preset_id')->references('id')->on('map_presets');
+            });
+        }
     }
 
     /**
--- database/migrations/2019_01_20_054102_recreate_map_preset_business_hours_table.php
+++ database/migrations/2019_01_20_054102_recreate_map_preset_business_hours_table.php
@@ -13,23 +13,25 @@ class RecreateMapPresetBusinessHoursTable extends Migration
      */
     public function up()
     {
-        Schema::create('map_preset_business_hours', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('map_preset_id')->index();
-            $table->integer('open_period_mins');
-            $table->integer('close_period_mins');
-            $table->boolean('wd_0')->default(false);
-            $table->boolean('wd_1')->default(false);
-            $table->boolean('wd_2')->default(false);
-            $table->boolean('wd_3')->default(false);
-            $table->boolean('wd_4')->default(false);
-            $table->boolean('wd_5')->default(false);
-            $table->boolean('wd_6')->default(false);
+        if(!Schema::hasTable('map_preset_hours')) {
+            Schema::create('map_preset_business_hours', function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('map_preset_id')->index();
+                $table->integer('open_period_mins');
+                $table->integer('close_period_mins');
+                $table->boolean('wd_0')->default(false);
+                $table->boolean('wd_1')->default(false);
+                $table->boolean('wd_2')->default(false);
+                $table->boolean('wd_3')->default(false);
+                $table->boolean('wd_4')->default(false);
+                $table->boolean('wd_5')->default(false);
+                $table->boolean('wd_6')->default(false);
 
-            $table->timestampsTz();
-            $table->softDeletesTz();
-            $table->foreign('map_preset_id')->references('id')->on('map_presets');
-        });
+                $table->timestampsTz();
+                $table->softDeletesTz();
+                $table->foreign('map_preset_id')->references('id')->on('map_presets');
+            });
+        }
     }
 
     /**
